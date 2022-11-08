c5c9a9a36daae01e876845244e2570ae533031bd
karenoliveiraw <93669330+karenoliveiraw@users.noreply.github.com>
2022-11-08 19:49:52 -0300

    Add files via upload

---
+++ UC15/02_vincular/vinc.js
@@ -0,0 +1,10 @@
+// comentarios 
+/* comentarios */
+
+document.write ("Olá, Karen Oliveira") 
+
+document.write ("<h2>" + "Olá, Karen Oliveira" + "</h2>") 
+
+document.write ('<h2>' + 'Olá, Karen Oliveira' + '</h2>') 
+
+document.write (`<h2> Olá, Karen Oliveira </h2>`) 
